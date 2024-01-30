7129abcf417f7a7032a0ac88975182431d057cb2
sebuahsemangat <mrsyntax25@gmail.com>
2024-01-30 15:00:08 +0700

    Tampilkan nama siswa di home

--- home.php
+++ home.php
@@ -5,6 +5,15 @@ if (!isset($_SESSION['siswa']))
 {
     header("location: index.php");
 }
+else
+{
+    //variabel session
+    $id_siswa = $_SESSION["siswa"];
+    //ambil data siswa
+    $qry_siswa = mysqli_query($koneksi, "SELECT * FROM siswa where id_siswa='$id_siswa'");
+    //pecah menjadi array
+    $data_siswa = mysqli_fetch_assoc($qry_siswa);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +23,6 @@ if (!isset($_SESSION['siswa']))
     <title>Home | Tes PHP</title>
 </head>
 <body>
-    <h2>Selamat Datang <?= $_SESSION["siswa"];?></h2>
+    <h2>Selamat Datang <?= $data_siswa["nama"];?></h2>
 </body>
 </html>
