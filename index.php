39e9377a061cbde7fba261c2aa4f00d4c7eca964
sebuahsemangat <mrsyntax25@gmail.com>
2024-02-19 14:20:25 +0700

    CSS Bootstrap Index

--- index.php
+++ index.php
@@ -8,11 +8,37 @@ include "koneksi.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Tes PHP</title>
+    <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+    <style>
+    /* CSS untuk menengahkan heading */
+    .center-heading {
+      text-align: center;
+    }
+    /* CSS untuk menengahkan secara vertikal dan horizontal */
+    .center-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh; /* Menggunakan 100% tinggi viewport */
+    }
+
+    footer {
+      text-align: center;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      background-color: #f8f9fa;
+      padding: 10px 0;
+    }
+  </style>
 </head>
 <body>
-    <h2>Selamat Datang di Aplikasi Tes PHP</h2>
-    <p>Silahkan Login untuk menggunakan aplikasi ini.</p>
-    <form action="" method="post">
+<div class="container">
+    <div class="row center-container">
+      <div class="col-md-6">
+        <h2 class="mt-5 center-heading">Selamat Datang di Aplikasi Tes PHP</h2>
+        <p class="center-heading">Silahkan Login untuk menggunakan aplikasi ini.</p>
+        <form action="" method="post">
         <?php
         //jika tombol login ditekan
         if(isset($_POST['login']))
@@ -41,27 +67,27 @@ include "koneksi.php";
             else
             {
                 //jika data tidak ditemukan
-                echo "NIS atau Password salah. Silahkan coba lagi!";
+                echo "<p class='btn btn-danger' style='width:100%'>NIS atau Password salah. Silahkan coba lagi!</p>";
             }
         }
         ?>
-        <table>
-            <tr>
-                <td>NIS</td>
-                <td>:</td>
-                <td><input type="text" name="nis" id=""></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td>:</td>
-                <td><input type="password" name="password" id=""></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td><input type="submit" value="Login" name="login"></td>
-            </tr>
-        </table>
-    </form>
+          <div class="mb-3">
+            <label for="username" class="form-label">NIS</label>
+            <input type="text" class="form-control" id="username" name="nis" required>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+          </div>
+          <button type="submit" class="btn btn-primary" name="login">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <div class="container">
+      <p>Apep Wahyudin</p>
+    </div>
+  </footer>
 </body>
 </html>
