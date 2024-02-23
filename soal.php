6f2abaa892b0d4ac69fe6c3f9e2ebf9d70f26cf0
sebuahsemangat <mrsyntax25@gmail.com>
2024-02-23 14:02:12 +0700

    Tambahkan data tables

--- soal.php
+++ soal.php
@@ -8,7 +8,9 @@
         echo "<h2>" . $data_ujian['nama_ujian'] . "</h2>";
     ?>
     <p><a href="home.php">Kembali</a></p>
-    <table>
+    <h4>List Soal</h4>
+    <table id="myTable" class="table table-striped table-bordered" style="width:100%">
+    <thead>
         <tr>
             <th>No</th>
             <th>Judul Soal</th>
@@ -16,6 +18,7 @@
             <th>Pengerjaan</th>
             <th></th>
         </tr>
+    </thead>
     <?php
     //ambil data soal berdasarkan id_ujian dan status aktif
     $qry_soal = mysqli_query($koneksi, "SELECT * FROM soal where id_ujian='$id_ujian' AND status='aktif'");
@@ -24,6 +27,7 @@
     while($data_soal=mysqli_fetch_assoc($qry_soal)){
         $no++;
     ?>
+    <tbody>
         <tr>
             <td><?= $no;?></td>
             <td><?= $data_soal['judul'];?></td>
@@ -64,4 +68,5 @@
     //akhir looping soal
     }
     ?>
+    </tbody>
     </table>
