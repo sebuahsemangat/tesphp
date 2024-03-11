2595334ea846d37a2ec8f6af4e0aafd7b950fdac
sebuahsemangat <mrsyntax25@gmail.com>
2024-03-11 11:23:35 +0700

    Hapus ujian

--- guru/hapus_ujian.php
+++ guru/hapus_ujian.php
@@ -1,16 +1,14 @@
 <?php
 include "../koneksi.php";
-$id_ujian=$_GET['id_ujian'];
-if(!isset($id_ujian) or $id_ujian=='' or $id_ujian==0){
+$id_ujian = $_GET['id_ujian'];
+if (!isset($id_ujian) or $id_ujian == '' or $id_ujian == 0) {
     echo "ERROR 404";
-}
-else {
+} else {
     $hapus_ujian = mysqli_query($koneksi, "DELETE FROM ujian WHERE id_ujian = '$id_ujian'");
-    if($hapus_ujian){
+    if ($hapus_ujian) {
         header("location: home.php");
         exit();
-    }
-    else {
+    } else {
         echo "Data gagal dihapus";
     }
 }
