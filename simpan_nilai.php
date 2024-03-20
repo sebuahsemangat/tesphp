b1a5e944e6c8f3893027bc0b85195f767c064c7b
sebuahsemangat <mrsyntax25@gmail.com>
2024-03-20 11:46:48 +0700

    Relasi ujian dan kelas

--- simpan_nilai.php
+++ simpan_nilai.php
@@ -9,19 +9,16 @@ $jawaban = $_POST['jawaban'];
 $qrysoal = mysqli_query($koneksi, "SELECT * FROM soal where id_soal='$id_soal' LIMIT 1");
 $datasoal = mysqli_fetch_assoc($qrysoal);
 $id_ujian = $datasoal["id_ujian"];
-$insert = mysqli_query($koneksi,"INSERT INTO hasil (id_soal, id_siswa, jawaban, nilai) VALUES ('$id_soal', '$id_siswa','$jawaban','$nilai')");
-if ($insert)
-{
+$insert = mysqli_query($koneksi, "INSERT INTO hasil (id_soal, id_siswa, jawaban, nilai) VALUES ('$id_soal', '$id_siswa','$jawaban','$nilai')");
+if ($insert) {
     // Mendapatkan URL dari halaman index.php
-$parent_url = $_SERVER['HTTP_REFERER'];
+    $parent_url = $_SERVER['HTTP_REFERER'];
 
-// Menentukan lokasi home.php
-$home_url = 'home.php?page=soal&id_ujian=' . $id_ujian;
+    // Menentukan lokasi home.php
+    $home_url = 'home.php?page=soal&id_ujian=' . $id_ujian;
 
-// Script JavaScript untuk mengubah lokasi parent frame
-echo "<script>window.parent.location.href = '$home_url';</script>";
-}
-else {
+    // Script JavaScript untuk mengubah lokasi parent frame
+    echo "<script>window.parent.location.href = '$home_url';</script>";
+} else {
     echo "Kirim gagal";
 }
-?>
--- ujian.php
+++ ujian.php
@@ -1,28 +1,32 @@
-<h2>Selamat Datang <?= $data_siswa["nama"];?></h2>
-            
-                <table id="myTable" class="table table-striped table-bordered" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>Nama Ujian</th>
-                            <th>Jumlah Soal</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-    <?php
-    //ambil data ujian berdasarkan kelas siswa dan statusnya aktif
-    $qry_ujian = mysqli_query($koneksi, "SELECT * FROM ujian WHERE id_kelas='$data_siswa[id_kelas]' AND status='aktif'");
-    // pecah menjadi array kemudian looping
-    while($data_ujian=mysqli_fetch_assoc($qry_ujian))
-    {
+<h2>Selamat Datang <?= $data_siswa["nama"]; ?></h2>
+
+<table id="myTable" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th>Nama Ujian</th>
+            <th>Jumlah Soal</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        //ambil data ujian berdasarkan kelas siswa dan statusnya aktif
+        $qry_ujian = mysqli_query($koneksi, "SELECT ujian.id_ujian, ujian.nama_ujian, COUNT(soal.id_soal) AS jumlah_soal
+    FROM ujian
+    LEFT JOIN ujian_kelas ON ujian.id_ujian = ujian_kelas.id_ujian
+    LEFT JOIN soal ON ujian.id_ujian = soal.id_ujian
+    WHERE ujian_kelas.id_kelas = '$data_siswa[id_kelas]' GROUP BY ujian.id_ujian, ujian.nama_ujian
+          ORDER BY ujian.id_ujian DESC");
+        // pecah menjadi array kemudian looping
+        while ($data_ujian = mysqli_fetch_assoc($qry_ujian)) {
         ?>
-                        <tr>
-                            <td><?= $data_ujian['nama_ujian'];?></td>
-                            <td>30</td>
-                            <td><?= "<a href='home.php?page=soal&id_ujian=" . $data_ujian['id_ujian'] . "'>Kerjakan</a>"?></td>
-                        </tr>
-    <?php
-        }    
-    ?>                        
-                    </tbody>
-                </table>
+            <tr>
+                <td><?= $data_ujian['nama_ujian']; ?></td>
+                <td><?= $data_ujian['jumlah_soal'];?></td>
+                <td><?= "<a href='home.php?page=soal&id_ujian=" . $data_ujian['id_ujian'] . "'>Kerjakan</a>" ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
