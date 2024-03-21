6dec3dc5c92524132827bbf121cf76f2bb74dc4a
sebuahsemangat <mrsyntax25@gmail.com>
2024-03-21 10:07:34 +0700

    Hapus ujian ditambah hapus ujian_kelas

--- guru/hapus_ujian.php
+++ guru/hapus_ujian.php
@@ -6,8 +6,11 @@ if (!isset($id_ujian) or $id_ujian == '' or $id_ujian == 0) {
 } else {
     $hapus_ujian = mysqli_query($koneksi, "DELETE FROM ujian WHERE id_ujian = '$id_ujian'");
     if ($hapus_ujian) {
+        $hapus_ujian_kelas = mysqli_query($koneksi, "DELETE FROM ujian_kelas WHERE id_ujian = '$id_ujian'");
+        if($hapus_ujian_kelas){
         header("location: home.php");
         exit();
+        }
     } else {
         echo "Data gagal dihapus";
     }
