3056cdc3cf907a4fd7dfeda75f9b425d619a8170
sebuahsemangat <mrsyntax25@gmail.com>
2024-06-14 09:52:04 +0700

    Perhitungan nilai di halaman ujian siswa

--- ujian.php
+++ ujian.php
@@ -46,14 +46,34 @@ if (!isset($_SESSION['siswa'])) {
 
             // Looping melalui hasil query
             while ($data_ujian = $result->fetch_assoc()) {
+                // Mengambil jumlah jawaban benar dari tabel hasil
+                $stmt_jawaban_benar = $koneksi->prepare("
+                    SELECT COUNT(*) AS jumlah_benar
+                    FROM hasil
+                    WHERE id_ujian = ? AND id_siswa = ? AND test_result = 'lulus'
+                ");
+                $stmt_jawaban_benar->bind_param("ii", $data_ujian['id_ujian'], $data_siswa['id_siswa']);
+                $stmt_jawaban_benar->execute();
+                $result_jawaban_benar = $stmt_jawaban_benar->get_result();
+                $data_jawaban_benar = $result_jawaban_benar->fetch_assoc();
+                $jumlah_benar = $data_jawaban_benar['jumlah_benar'];
+
+                // Menghitung nilai akhir
+                $nilai = 0;
+                if ($data_ujian['jumlah_soal'] > 0) {
+                    $nilai = ($jumlah_benar / $data_ujian['jumlah_soal']) * 100;
+                }
+
+                // Menampilkan data ujian dan nilai
         ?>
                 <tr>
                     <td><?= htmlspecialchars($data_ujian['nama_ujian'], ENT_QUOTES, 'UTF-8'); ?></td>
                     <td><?= htmlspecialchars($data_ujian['jumlah_soal'], ENT_QUOTES, 'UTF-8'); ?></td>
-                    <td>Di sini nilainya</td>
+                    <td><?= htmlspecialchars(number_format($nilai, 0), ENT_QUOTES, 'UTF-8'); ?></td>
                     <td><a href='home.php?page=soal&id_ujian=<?= htmlspecialchars($data_ujian['id_ujian'], ENT_QUOTES, 'UTF-8'); ?>'>Kerjakan</a></td>
                 </tr>
         <?php
+                $stmt_jawaban_benar->close();
             }
             $stmt->close();
         } else {
