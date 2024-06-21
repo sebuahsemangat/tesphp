d66a377de1e4c24aaaf918e0184dcf4092dcf909
sebuahsemangat <mrsyntax25@gmail.com>
2024-06-21 14:06:35 +0700

    Perbaikan koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,9 +3,17 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "tesphp";
-$koneksi = new mysqli($servername, $username, $password, $dbname);
 
-// Periksa koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+// Meningkatkan pelaporan kesalahan
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    // Membuat koneksi
+    $koneksi = new mysqli($servername, $username, $password, $dbname);
+    // Mengatur karakter set ke utf8mb4 untuk mendukung karakter khusus
+    $koneksi->set_charset("utf8mb4");
+} catch (Exception $e) {
+    // Mencatat kesalahan ke file log dan menampilkan pesan kesalahan generik ke pengguna
+    error_log($e->getMessage());
+    exit('Koneksi ke database gagal.');
 }
